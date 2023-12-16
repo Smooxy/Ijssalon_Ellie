75f0e71657ef9000dc1831a4ff5d4bb5591f9289
Ahmed <A_Bouyajdad@Outlook.com>
2023-12-16 21:14:50 +0100

    Een onnodige regel verwijderd print(el.lower())

--- tijdelijk.py
+++ tijdelijk.py
@@ -9,9 +9,8 @@ reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter â€“ slechts â‚
 reclame_tekst2 = reclame_tekst[:63]
 reclame_tekst3 = reclame_tekst2.upper()
 reclame_tekst4 = reclame_tekst3.split()
-for el in reclame_tekst4:
-    print(el.lower())
-    if len(el) >= 5:
+for el in reclame_tekst4:                          
+    if len(el) >= 5: 
         print(el.upper())
     else:
-        print(el.lower())    
+        print(el.lower())
